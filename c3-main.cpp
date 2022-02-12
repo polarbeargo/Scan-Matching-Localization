2de3c982f30ba09459a95d70c9a100e362bcb500
alice <bow1226@gmail.com>
2022-02-12 12:04:31 +0800

    feat: Implement ICP Scan Matching algorithm

--- c3-main.cpp
+++ c3-main.cpp
@@ -99,6 +99,37 @@ void drawCar(Pose pose, int num, Color color, double alpha, pcl::visualization::
 	renderBox(viewer, box, num, color, alpha);
 }
 
+Eigen::Matrix4d ICP(PointCloudT::Ptr target, PointCloudT::Ptr source, Pose startingPose, int iterations){
+
+    Eigen::Matrix4d transformation_matrix = Eigen::Matrix4d::Identity ();
+
+    // align source with starting pose
+      Eigen::Matrix4d initTransform = transform2D(startingPose.theta, startingPose.position.x, startingPose.position.y);
+      PointCloudT::Ptr transformSource (new PointCloudT); 
+      pcl::transformPointCloud (*source, *transformSource, initTransform);
+
+    pcl::console::TicToc time;
+      time.tic ();
+      pcl::IterativeClosestPoint<PointT, PointT> icp;
+      icp.setMaximumIterations (iterations);
+      icp.setInputSource (transformSource);
+      icp.setInputTarget (target);
+      PointCloudT::Ptr cloud_icp (new PointCloudT);  // ICP output point cloud
+      icp.align (*cloud_icp);
+
+      if (icp.hasConverged ())
+      {
+          std::cout << "\nICP has converged, score is " << icp.getFitnessScore () << std::endl;
+          transformation_matrix = icp.getFinalTransformation ().cast<double>();
+          transformation_matrix =  transformation_matrix * initTransform;
+          return transformation_matrix;
+      }
+      cout << "WARNING: ICP did not converge" << endl;
+
+    return transformation_matrix;
+
+}
+
 int main(){
 
 	auto client = cc::Client("localhost", 2000);
