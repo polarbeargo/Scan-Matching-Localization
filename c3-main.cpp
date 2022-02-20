3873b16aa3e02b703af4e2f7dd22f965e726e4fd
alice <bow1226@gmail.com>
2022-02-20 09:14:47 +0800

    style: Format

--- c3-main.cpp
+++ c3-main.cpp
@@ -149,7 +149,7 @@ Eigen::Matrix4d NDT(PointCloudT::Ptr target, PointCloudT::Ptr source, Pose start
 	time.tic();
 	pcl::NormalDistributionsTransform<pcl::PointXYZ, pcl::PointXYZ> ndt;
 	Eigen::Matrix4f init_guess = transform3D(startingPose.rotation.yaw, startingPose.rotation.pitch, startingPose.rotation.roll, startingPose.position.x, startingPose.position.y, startingPose.position.z).cast<float>();
-	ndt.setTransformationEpsilon (1e-6);
+	ndt.setTransformationEpsilon(1e-6);
 	// Setting max number of registration iterations.
 	int max_iterations = 5;
 	ndt.setMaximumIterations(max_iterations);
